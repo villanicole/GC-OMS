d662455316eb643f6346316fe4c7cdd4906ef45c
Eugene <eugenelapitan041101@gmail.com>
2023-06-15 21:12:25 +0800

    adviser
    adviser

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>GC-OMS</title>
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/form-elements.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+
+
+  <!-- <link rel="stylesheet" href="fonts/Oswald/Oswald.ttf">
+ <link rel="stylesheet" href="fonts/Pacifico/Pacifico-Regular.ttf">
+ <link rel="stylesheet" href="fonts/Oswald/Oswald.ttf"> -->
+
+  <!-- <script src="js/jquery.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="index.js"></script> -->
+
+  <!-- Favicon and touch icons -->
+  <link rel="shortcut icon" href="assets\img\logo\favicon.png">
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+
+  <!-- 
+ <link rel="stylesheet" type="text/css" href="index.css"> -->
+
+</head>
+
+<body style="background-image: url('assets/img/backgrounds/4.jpg'); background-size: 110% auto; background-repeat: no-repeat">
+
+  <!-- <div class="container-fluid text-white">
+
+ <nav class="row py-4">
+  <div class="col-lg-9 col-md-9 col-12  logo">
+   <h1><span class="colorchange">GC-OMS</span></h1><br>
+   <span class="edina_tm_animation_text_word"></span> -->
+
+  <!-- </nav> -->
+
+
+
+  <!-- <div class="row main-content ">
+    <div class="col-md-9 col-12 leftside"> </div>
+
+    <div class="col-md-3 col-12 rightside d-flex justify-content-center align-items-center">
+      <div class="right-content w-50 ">
+        <div class="row">
+          <div class="col-sm-6 col-sm-offset-3 form-box">
+            <div class="form-top">
+              <div class="form-top-left">
+                <h3>Login to our site</h3>
+                <p>Enter your username and password to log on:</p>
+              </div>
+              <div class="form-top-right">
+                <i class="fa fa-lock"></i>
+              </div>
+            </div>
+            <div class="form-group form-popup" id="myForm">
+              <form action="login.php" class="form-container" method="post">
+                <div class="form-group">
+                  <label class="sr-only" for="form-username">Username</label>
+                  <input class="form-control" type="text" placeholder="Enter Username" name="username" required>
+                </div>
+                <div class="form-group">
+                  <label class="sr-only" for="form-password">Password</label>
+                  <<input class="form-control" type="password" placeholder="Enter Password" name="password" required>
+                </div>
+                <button type="submit" class="btn" name="login" value="Login">Sign in!</button>
+              </form>
+            </div>
+          </div>
+        </div>
+
+         <div class="form-group form-popup" id="myForm">
+          <form action="login.php" class="form-container" method="post">
+            <h1>Login</h1>
+
+            <label for="username"><b>Username</b></label>
+            <input class="form-control" type="text" placeholder="Enter Username" name="username" required>
+
+            <label for="psw"><b>Password</b></label>
+            <input class="form-control" type="password" placeholder="Enter Password" name="password" required>
+
+            <input type="submit" class="btn" name="login" value="Login">
+            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+          </form>
+        </div> -->
+<!-- 
+  <script>
+    function openForm() {
+      document.getElementById("myForm").style.display = "block";
+    }
+
+    function closeForm() {
+      document.getElementById("myForm").style.display = "none";
+    }
+  </script> -->
+
+  <!-- </div>
+  </div>
+  </div> -->
+
+<div class="container">
+
+  <div class="top-content">
+
+    <div class="inner-bg">
+      <div class="container">
+        <!-- <div class="row">
+          <div class="col-sm-8 col-sm-offset-2 text">
+            <h1><strong>Bootstrap</strong> Login Form</h1>
+            <div class="description">
+              <p>
+                This is a free responsive login form made with Bootstrap.
+                Download it on <a href="http://azmind.com"><strong>AZMIND</strong></a>, customize and use it as you
+                like!
+              </p>
+            </div>
+          </div>
+        </div> -->
+        <div class="col-sm-6 col-sm-offset-3 form-box">
+          <div class="form-top">
+          <h3>Gordon College OJT</h3>
+          <h3>Management System</h3>
+            <div class="form-top-left"> 
+              <!-- <p>Enter your username and password to log on:</p> -->
+            </div>
+            <div class="form-top-right">
+              
+              <!-- <i class="fa fa-lock"></i> -->
+            </div>
+          </div>
+          <div class="form-group form-popup form-bottom" id="myForm">
+            <form action="login.php" class="form-container" method="post">
+              <div class="form-group">
+                <label class="sr-only" for="form-username">Username</label>
+                <input class="form-control" type="text" placeholder="Enter Username" name="username" required>
+              </div>
+              <div class="form-group">
+                <label class="sr-only" for="form-password">Password</label>
+                <input class="form-control" type="password" placeholder="Enter Password" name="password" required>
+              </div>
+              <button type="submit" class="btn" name="login" value="Login">Sign in!</button>
+            </form>
+          </div>
+        </div>
+        <!-- <div class="row">
+          <div class="col-sm-6 col-sm-offset-3 social-login">
+            <h3>...or login with:</h3>
+            <div class="social-login-buttons">
+              <a class="btn btn-link-2" href="#">
+                <i class="fa fa-facebook"></i> Facebook
+              </a>
+              <a class="btn btn-link-2" href="#">
+                <i class="fa fa-twitter"></i> Twitter
+              </a>
+              <a class="btn btn-link-2" href="#">
+                <i class="fa fa-google-plus"></i> Google Plus
+              </a>
+            </div>
+          </div>
+        </div> -->
+      </div>
+    </div>
+
+  </div>
+</div>
+
+  <!-- <script src="assets/js/jquery-1.11.1.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/js/jquery.backstretch.min.js"></script>
+  <script src="assets/js/scripts.js"></script> -->
+
+</body>
+
+</html>
